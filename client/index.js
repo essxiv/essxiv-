aae5158cac95e43fb3fd8e10bd344bf69990c38f
eSsXiV <essxiv@gmail.com>
2017-12-17 15:36:37 -0800

    added react-router-redux, history creation, connecting with redux store while using react-router

--- client/index.js
+++ client/index.js
@@ -1,15 +1,24 @@
+// Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
+
+// Components
+import Home from './components/Home';
+import Experience from './components/Experience';
 import store from '../redux/store';
 
+// history creation with redux-store
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
-
-  <Provider store={store}>
-    <Home />
-  </Provider>,
-
-  document.getElementById('root')
-
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={Experience} />
+        </Router>
+    </Provider>,
+    document.getElementById('root')
 );
