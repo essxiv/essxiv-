c7ceec85e0e1a8e9ca9cc7d8c50e5e687e98d391
eSsXiV <essxiv@gmail.com>
2017-12-17 15:37:24 -0800

    aded watch for re-bundle, added mocha/chai test task

--- gulpfile.js
+++ gulpfile.js
@@ -8,20 +8,26 @@ const concat = require('gulp-concat');
 const cssmin = require('gulp-cssmin');
 const less = require('gulp-less');
 const watch = require('gulp-watch');
+const mocha = require('gulp-mocha');
 
 //  file paths
 const webpackConfig = require('./webpack.config.js');
 const lessStyles = './client/styles/components';
 
+// Watch and Recompile
 gulp.task('watch', () => {
-
     let watcher = gulp.watch(lessStyles + '/*.less', [ 'less' ])
-
     watcher.on('change', (event) => {
         console.log('File: ' + event.path + ' was changed!')
     });
 });
 
+gulp.task('test', () => {
+    return gulp.src('./test/**/*.js', { read: false })
+    .pipe((mocha({ reporter: 'spec' })))
+})
+
+// Less into MinCss
 gulp.task('less', () => {
     return gulp.src(lessStyles + '/*.less')
     .pipe(less({
@@ -32,16 +38,13 @@ gulp.task('less', () => {
     .pipe(gulp.dest('./client/dist'));
 })
 
+// Webpack Bundler
 gulp.task('webpack', () => {
     return gulp.src('./webpack.config.js')
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('./client/dist'));
 })
 
-// gulp.task('run-dev', () => {
-//
-// })
-
-gulp.task('default', [ 'less', 'webpack', 'watch' ], () => {
+gulp.task('default', [ 'test', 'less', 'webpack', 'watch' ], () => {
     console.log('Gulp is running correctly..');
 });
